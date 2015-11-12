187fd2621de3bf22510169ba07dc97d3199ca9a8
aravindsankar91 <aravindsankar91@users.noreply.github.com>
2015-11-12 17:08:06 +0530

    Update pipeline_input.py

--- scripts/pipeline_input.py
+++ scripts/pipeline_input.py
@@ -3,7 +3,7 @@ import os
 from subprocess import call
 #This is the script that is run at the command line for executing the pipeline.
 #Add as many read files as needed here. This should automatically run the script for each one.
-cmd ="python pipeline.py Sample1_1.fastq"
+cmd ="python pipeline.py readset_1.fastq"
 call(cmd,shell=True)
-cmd ="python pipeline.py Sample2_1.fastq"
+cmd ="python pipeline.py readset2_1.fastq"
 call(cmd,shell=True)
