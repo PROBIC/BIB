ca864ef0d56fb757dc154bf66bc2ee9872d084a8
aravindsankar91 <aravindsankar91@users.noreply.github.com>
2015-11-18 19:42:28 +0530

    Update BIB_prepare_index.py

--- scripts/BIB_prepare_index.py
+++ scripts/BIB_prepare_index.py
@@ -1,6 +1,11 @@
 import sys
 import os
 from subprocess import call
-#This step constructs the reference index to which the reads are eventually aligned. The input for this script are the core genomes while the output is a Bowtie index called core_genome_index.
-cmd ="bowtie2-build core_genomes.fasta core_genome_index"
+#This step constructs the reference index to which the reads are eventually aligned. 
+#The input for this script are the core genomes while the output is a Bowtie2 reference index.
+#The reference index produced is of the same name as the input core genomes.
+argu = sys.argv[1]
+fname = argu.split(".")
+core_genomes = fname[0]
+cmd ="bowtie2-build "+core_genomes+".fasta "+core_genomes
 call(cmd,shell=True)
