027602ca94f185d1dfef0199ca1286afa5b60ad3
Antti Honkela <antti.honkela@hiit.fi>
2015-11-19 16:12:52 +0200

    cleaning up old code

--- scripts/pipeline_input.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-from subprocess import call
-#This is the script that is run at the command line for executing the pipeline.
-#Add as many read files as needed here. This should automatically run the script for each one.
-cmd ="python pipeline.py readset_1.fastq"
-call(cmd,shell=True)
-cmd ="python pipeline.py readset2_1.fastq"
-call(cmd,shell=True)
