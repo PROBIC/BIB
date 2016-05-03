61d957ed052208575cc410595b53ab3f5787bb75
Antti Honkela <antti.honkela@hiit.fi>
2016-05-03 16:07:30 +0300

    combined plot for different clusterings

--- sepi_example/analyse_results.py
+++ sepi_example/analyse_results.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 REFERENCEPATH = "/cs/work/scratch/ahonkela/bib/sepi/reference"
 READPATH = "/cs/work/scratch/ahonkela/bib/sepi/Meric2015"
 SAMPLEINFO = "/cs/work/scratch/ahonkela/bib/sepi/Meric et al Isolate Info.xlsx"
-CLUSTER_INDEX = 1
 
 CLUSTER_SIZES = [3, 11]
 
@@ -45,48 +44,51 @@ def read_clusters(index):
 def plot_boxplots(x1, x2):
     f, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(87.0/25.4, 70.0/25.4))
     plt.rcParams.update({'font.size': 6.0})
-    ax1.boxplot(x1.flatten())
-    ax2.boxplot(x2.flatten())
+    ax1.boxplot(x1)
+    ax2.boxplot(x2)
     for x in [ax1, ax2]:
-        x.set_xticks(())
+        x.set_xticklabels(("3 clusters", "11 clusters"))
         x.set_ylabel("Estimated relative abundance")
     ax1.set_title("Correct cluster")
     ax2.set_title("Other clusters")
 
+def read_results(index):
+    files = os.listdir(READPATH)
+    samples = [x[:-18] for x in files if x.endswith("_1_sequence.txt.gz")]
 
-files = os.listdir(READPATH)
-samples = [x[:-18] for x in files if x.endswith("_1_sequence.txt.gz")]
+    ids = read_sample_mapping()
+    clusters = read_clusters(index)
 
-ids = read_sample_mapping()
-clusters = read_clusters(CLUSTER_INDEX)
+    thetas = []
+    trueclusters = np.zeros(len(samples), np.int64)
+    trueclthetas = np.zeros(len(samples))
+    otherthetas = np.zeros((len(samples), CLUSTER_SIZES[index]-1))
+    for i, s in enumerate(tqdm(samples)):
+        filestem = READPATH + "/" + s
+        outfile = filestem + "_BAPS%d.m_alphas" % index
+        trfile = filestem + "_BAPS%d.tr" % index
 
-thetas = []
-trueclusters = np.zeros(len(samples), np.int64)
-trueclthetas = np.zeros(len(samples))
-otherthetas = np.zeros((len(samples), CLUSTER_SIZES[CLUSTER_INDEX]-1))
-for i, s in enumerate(tqdm(samples)):
-    filestem = READPATH + "/" + s
-    outfile = filestem + "_BAPS%d.m_alphas" % CLUSTER_INDEX
-    trfile = filestem + "_BAPS%d.tr" % CLUSTER_INDEX
+        trs = np.array(read_table(trfile))
+        names = trs[:,1]
+        allthetas = np.array(read_table(outfile)).astype(np.float64)
+        thetas.append(allthetas[1:,0])
+        trueclusters[i] = clusters[ids[s]]
+        trueclthetas[i] = allthetas[trueclusters[i],0]
+        otherthetas[i, :] = np.delete(allthetas[1:,0], trueclusters[i]-1)
 
-    trs = np.array(read_table(trfile))
-    names = trs[:,1]
-    allthetas = np.array(read_table(outfile)).astype(np.float64)
-    thetas.append(allthetas[1:,0])
-    trueclusters[i] = clusters[ids[s]]
-    trueclthetas[i] = allthetas[trueclusters[i],0]
-    otherthetas[i, :] = np.delete(allthetas[1:,0], trueclusters[i]-1)
+    refstrains = [int(x) for x in trs[:,1]]
+    samplestrains = [ids[x] for x in samples]
+    I = np.array(list(filter(lambda x: samplestrains[x] not in refstrains, range(len(samples)))))
+    return trueclthetas[I], otherthetas[I,:]
 
-thetas = np.stack(thetas, axis=1).T
+x1, y1 = read_results(0)
+x2, y2 = read_results(1)
 
-refstrains = [int(x) for x in trs[:,1]]
-samplestrains = [ids[x] for x in samples]
-I = np.array(list(filter(lambda x: samplestrains[x] not in refstrains, range(len(samples)))))
+trueclthetas = [x1, x2]
+otherthetas = [y1, y2]
 
 plot_boxplots(trueclthetas, otherthetas)
-plt.savefig("sepi_results_baps%d.pdf" % CLUSTER_INDEX)
+plt.savefig("sepi_results.pdf")
 
-plot_boxplots(trueclthetas[I], otherthetas[I,:])
-plt.savefig("sepi_results_baps%d_filtered.pdf" % CLUSTER_INDEX)
-
-print(np.sum(np.max(otherthetas[I,:], 1) < trueclthetas[I]), '/', len(I))
+for x, y in zip(trueclthetas, otherthetas):
+    print(np.sum(np.max(y, 1) < x), '/', len(x))
